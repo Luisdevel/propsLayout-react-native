3550e04a6cd999d8eda00031d14090f7ba600d58
Luis Henrique <luishenrique.mf01@gmail.com>
2021-01-25 20:38:59 -0300

    commit initial

---
+++ android/app/src/main/java/com/awesomeproject/CalendarModule.java
@@ -0,0 +1,62 @@
+package com.AwesomeProject; // replace com.your-app-name with your app’s name
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import android.util.Log;
+
+
+public class CalendarModule extends ReactContextBaseJavaModule {
+   CalendarModule(ReactApplicationContext context) {
+       super(context);
+   }
+}
+
+const { CalendarModule } = ReactNative.NativeModule;
+
+@ReactMethod
+public void createCalendarEvent(String name, String location) {
+   Log.d("CalendarModule", "Create event called with name: " + name
+   + " and location: " + location);
+}
+
+@ReactMethod(isBlockingSynchronousMethod = true)
+
+public class MyAppPackage implements ReactPackage {
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+ 
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+ 
+        modules.add(new CalendarModule(reactContext));
+ 
+        return modules;
+    }
+ 
+ }
+
+ @Override
+  protected List<ReactPackage> getPackages() {
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    List<ReactPackage> packages = new PackageList(this).getPackages();
+    // below MyAppPackage is added to the list of packages returned
+    packages.add(new MyAppPackage());
+    return packages;
+  }
